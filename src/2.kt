bb916876216d6c30a645c17d0aeabe44905e4941
Rodya187 <r.dubanov@vibelab.ru>
2025-09-02 19:40:20 +0300

    Fix if to when

--- src/2.kt
+++ src/2.kt
@@ -1,22 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 fun main() {
 
     println("Введите баллы")
-     val ball: Int = readln()?.toInt()?:0
-    if (ball in 90..100) {
-        println("Оценка А")
-    }
-    else if (ball in 75..89) {
-        println("Оценка B")
-    }
-    else if (ball in 50..74){
-        println("Оценка C")
-    }
-    else if (ball in 30..49) {
-        println("Оценка D")
-    }
-    else if (ball in 1..29) {
-         println ("Оценка F")
+    val ball: Int = readln()?.toInt()?:0
+    when (ball) {
+        in 90..100 -> {
+            println("Оценка А")
+        }
+        in 75..89 -> {
+            println("Оценка B")
+        }
+        in 50..74 -> {
+            println("Оценка C")
+        }
+        in 30..49 -> {
+            println("Оценка D")
+        }
+        in 1..29 -> {
+            println("Оценка F")
+        }
     }
 }
